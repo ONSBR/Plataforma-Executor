c5fec6e5112276f5edf9119644c1d86130b7d4ad
INTRANET\fca <fernando.andrade@pitang.com>
2017-12-26 08:31:59 -0300

    correção do executor para configuração de caminho do run

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.PORT = 8085;
-config.pathExecuteWorker = "plataforma-sdk/worker/run.js";
+config.pathExecuteWorker = "../../worker/run.js";
 config.processMemoryUrl = "http://localhost:9091/";
 
 module.exports = config;

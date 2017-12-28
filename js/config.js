a322cfb24b2afbc97bd400e0af4d483272db7705
Hugo Menezes <hugomenezes@gmail.com>
2017-12-28 15:53:47 -0300

    variavel para o docker

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
 var config = {};
 
 config.PORT = 8085;
+
+const processMemoryHost = process.env.PROCESS_MEMORY_HOST || "localhost";
+
 config.pathExecuteWorker = "../../worker/run.js";
-config.processMemoryUrl = "http://localhost:9091/";
+config.processMemoryUrl = "http://" + processMemoryHost + ":9091/";
 
 module.exports = config;

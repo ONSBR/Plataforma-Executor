455abfbce162de819988f3cefb7ce9b955d85849
Hugo Menezes <hugomenezes@gmail.com>
2017-12-28 18:10:36 -0300

    caminho do worker para o docker

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ config.PORT = 8085;
 
 const processMemoryHost = process.env.PROCESS_MEMORY_HOST || "localhost";
 
-config.pathExecuteWorker = "../../worker/run.js";
+config.pathExecuteWorker = process.env.WORKER_PATH || "../../worker/run.js";
 config.processMemoryUrl = "http://" + processMemoryHost + ":9091/";
 
 module.exports = config;

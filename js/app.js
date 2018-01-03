a1321c662132fe3ede8a1fc6b01a8a568aaa6823
INTRANET\fca <fernando.andrade@pitang.com>
2018-01-03 13:20:15 -0300

    Alterado para reprodução

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ var cmd = require('node-command-line');
 
 var Contexto = require("plataforma-core/Contexto");
 var DataSet = require("plataforma-core/DataSet");
+var Reproducao = require("plataforma-core/Reproducao");
 var CoreRepository = require("plataforma-sdk/services/CoreRepository");
+var utils = require("plataforma-sdk/utils");
 
 
 // Dependencies
@@ -18,9 +20,18 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = config.PORT;
 
+const eventReproduction = "reproduction";
+
+var coreRepository = new CoreRepository();
+
 // Set up the Express application to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.get("/executor", function(req, res) {
+  res.send("OK");
+});
 
 /**
  * Recebe os eventos para serem enviados ao executor,
@@ -33,28 +44,105 @@ app.post("/executor", function(req, res) {
 
   //TODO cria o contexto e manda para processmemory
   var evento = req.body;
+
+  var systemEvent = utils.getSystemEvent(evento.name);
+  if (systemEvent == eventReproduction) {
+    executeReprodutionEvent(evento);
+  } else {
+    executeBusinessEvent(evento);
+  }
+  
+  res.send("OK");
+});
+
+function executeReprodutionEvent(evento) {
+
+  var repData = evento.payload;
+  if (repData.instanciaOriginal) {
+    executeReprodutionByInstance(repData.instanciaOriginal);
+  } else {
+    var entidadeId = repData.entidadeId;
+    var entidadeType = repData.entidadeType;
+
+    // TODO obtém a instância apartir dos dados da entidade
+  }
+
+}
+
+function executeReprodutionByInstance(instanceId, evento) {
+
+  var instance = coreRepository.getProcessInstance(instanceId);
+  console.log("___instance___" + instanceId + "/ " + JSON.stringify(instance));
+  var processName = instance.processo;
+
+  var urlGetProcessMemory = config.processMemoryUrl + processName + "/" + instanceId + "/head";
+  var client = new Client();
+
+  client.get(urlGetProcessMemory, function (data, response) {
+    
+    var contexto = data;
+    var eventoReproducao = contexto.evento;
+
+    var instanciaOriginal = eventoReproducao.instancia;
+
+    var instanciaReproducao = guid();
+    var reproducaoId = guid();
+    
+    eventoReproducao.reproducao = reproducaoId;
+    eventoReproducao.instancia = instanciaReproducao;
+
+    contexto.eventoSaida = null;
+    contexto.instancia = instanciaReproducao;
+
+    var reproducao = new Reproducao(reproducaoId, instanciaOriginal, instanciaReproducao, new Date(), eventoReproducao.responsavel);
+
+    var urlMemoryCreate = config.processMemoryUrl + processName + "/" + contexto.instancia + "/create";
+    
+    console.log("urlMemoryCreateReprocess: " +urlMemoryCreate);
+    
+    var args = { data: contexto, headers: { "Content-Type": "application/json" } };
+
+    var reqExec = client.post(urlMemoryCreate, args, function (data, response) {
+
+      coreRepository.addProcessInstance(data.instanceId, eventoReproducao.processName, eventoReproducao.data, eventoReproducao.responsavel);
+
+      coreRepository.addReproduction(reproducao);
+  
+      console.log("Contexto salvo na memória de processo com sucesso." + data.instanceId);
+      execute(contexto.instancia);
+    });
+    reqExec.on('error', function (err) {
+      console.log('request error', err);
+    });
+
+  });
+
+}
+
+function executeBusinessEvent(evento) {
+  
   var contexto = new Contexto();
   contexto.instancia = evento.instancia;
   contexto.evento = evento;
+  contexto.dataDeReferencia = evento.dataRef;
   contexto.dataSet = new DataSet();
 
   // TODO ainda não está recuperando o dataset
-    
   if (!contexto.instancia) {
-
+    
     var args = { data: contexto, headers: { "Content-Type": "application/json" } };
   
     contexto.instancia = guid();
+    evento.instancia = contexto.instancia;
 
     var urlMemoryCreate = config.processMemoryUrl + evento.processName + "/" + contexto.instancia + "/create";
 
     console.log("urlMemoryCreate: " +urlMemoryCreate);
- 
+  
     var client = new Client();
     var reqExec = client.post(urlMemoryCreate, args, function (data, response) {
 
-      var coreRepository = new CoreRepository();
-      coreRepository.addProcessInstance(data.instanceId, evento.processName, evento.data);
+      coreRepository.addProcessInstance(data.instanceId, evento.processName, evento.data, evento.responsavel);
   
       console.log("Contexto salvo na memória de processo com sucesso." + data.instanceId);
       execute(contexto.instancia);
@@ -65,9 +153,8 @@ app.post("/executor", function(req, res) {
   } else {
     execute(contexto.instancia);
   }
-  
-  res.send("OK");
-});
+
+}
 
 function execute(instanceId) {
   var commandLine = "node " + config.pathExecuteWorker + " " + instanceId;

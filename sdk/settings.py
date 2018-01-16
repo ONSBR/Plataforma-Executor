1a7a600d6da7acc599a1012e7de4f91739d0424f
Fabio Gibson <fabiogibson@hotmail.com>
2018-01-16 19:34:08 -0200

    correct process memory port

--- sdk/settings.py
+++ sdk/settings.py
@@ -2,7 +2,7 @@ import os
 
 
 PROCESS_MEMORY_URL = f"http://{os.getenv('PROCESS_MEMORY_URL', 'process_memory')}"
-PROCESS_MEMORY_PORT = os.getenv('PROCESS_MEMORY_PORT', 80)
+PROCESS_MEMORY_PORT = os.getenv('PROCESS_MEMORY_PORT', 9091)
 
 
 COREAPI_URL = f"http://{os.getenv('COREAPI_URL', 'apicore')}"

e15c06d5411d95ddf890b8ead206a20ec39b614b
Fabio Gibson <fabiogibson@hotmail.com>
2018-01-17 21:06:03 -0200

    refactorin

--- runner/tests/test_docker_processor.py
+++ runner/tests/test_docker_processor.py
@@ -26,10 +26,10 @@ def test_execute_container_aborts_if_event_has_not_operation(mock_get_operation_
 @mock.patch('sdk.coreapi.get_operation_by_event')
 @mock.patch('sdk.process_memory.create_memory')
 @mock.patch('sdk.coreapi.create_process_instance')
-def test_execute_creates_process_instance_if_no_instanceid(mock_create_process_instance,
-                                                           mock_create_process_memory,
-                                                           mock_get_operation_by_event,
-                                                           mock_containers):
+def test_execute_creates_process_instance(mock_create_process_instance,
+                                          mock_create_process_memory,
+                                          mock_get_operation_by_event,
+                                          mock_containers):
     docker_processor = DockerProcessor()
     operation = {"container": "container"}
     mock_get_operation_by_event.return_value = operation
@@ -47,28 +47,7 @@ def test_execute_creates_process_instance_if_no_instanceid(mock_create_process_i
 @mock.patch('sdk.coreapi.get_operation_by_event')
 @mock.patch('sdk.process_memory.create_memory')
 @mock.patch('sdk.coreapi.create_process_instance')
-def test_execute_creates_process_instance_if_no_instanceid(mock_create_process_instance,
-                                                         mock_create_process_memory,
-                                                         mock_get_operation_by_event,
-                                                         mock_containers):
-    docker_processor = DockerProcessor()
-    operation = {"container": "container"}
-    mock_get_operation_by_event.return_value = operation
-    mock_create_process_instance.return_value = None
-
-    # act
-    docker_processor.process(models.Event(name="event"))
-
-    # assert
-    mock_create_process_instance.assert_called_with(operation)
-    mock_create_process_memory.assert_not_called()
-
-
-@mock.patch('docker.client.ContainerCollection')
-@mock.patch('sdk.coreapi.get_operation_by_event')
-@mock.patch('sdk.process_memory.create_memory')
-@mock.patch('sdk.coreapi.create_process_instance')
-def test_execute_creates_process_memory_if_no_instanceid(mock_create_process_instance,
+def test_execute_creates_process_memory(mock_create_process_instance,
                                                          mock_create_process_memory,
                                                          mock_get_operation_by_event,
                                                          mock_containers):
@@ -109,6 +88,3 @@ def test_execute_new_instance(mock_create_process_instance,
         detach=True,
         stdout=True,
     )
-
-
-

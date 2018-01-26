59c5a6c9419c54deabf0e29173ca7df160be8bec
Philippe Moneda <philippemoneda@gmail.com>
2018-01-26 15:52:10 -0200

    adicionar novos campos no evento

--- sdk/models.py
+++ sdk/models.py
@@ -5,6 +5,9 @@ class Event:
     def __init__(self, name, **kwargs):
         self.name = name
         self.instance_id = kwargs.pop('instanceId', None)
+        self.reference_date = kwargs.pop('referenceDate', None)
+        self.reproduction = kwargs.pop('reproduction', dict())
+        self.reprocess = kwargs.pop('reprocess', dict())
         self.payload = kwargs.pop('payload', dict())
 
 

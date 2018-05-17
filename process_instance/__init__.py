6c6d8429be21b260085339d1a80611b87dab1c86
Philippe Moneda <philippemoneda@gmail.com>
2018-05-17 16:06:52 -0300

    adiciona logs

--- process_instance/__init__.py
+++ process_instance/__init__.py
@@ -24,8 +24,9 @@ def create(event):
             event.instance_id)
     else:
         log(f"Creating new process instance to respond event {event.name}")
-        process_instance = coreapi.create_process_instance(
-            operation, event.name)
+        log(operation)
+        process_instance = coreapi.create_process_instance(operation, event.name)
+        log(process_instance)
         if not process_memory.create_memory(process_instance, event):
             log(
                 """

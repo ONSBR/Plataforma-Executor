1f8953d131dd88dd3736722e5914b468b5908ccc
Lucas Thimoteo <lucas.thimoteo@radixeng.com.br>
2020-02-13 14:18:24 -0300

    Bug fix with process_memory integration

--- sdk/process_memory.py
+++ sdk/process_memory.py
@@ -5,20 +5,20 @@ from sdk.utils import HttpClient
 
 def commit(id, data):
     result = HttpClient.post(
-        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/memory/{id}/commit", data)
+        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/{id}/commit", data)
     return not result.has_error
 
 
 def create_memory(process_instance_id, event):
     result = HttpClient.post(
-        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/memory/{process_instance_id}", { "event": event.__dict__})
+        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/{process_instance_id}", { "event": event})
 
     return not result.has_error
 
 def first_commit(instance_id):
     """ return first commit from instance id  """
     result = HttpClient.get(
-        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/memory/{instance_id}/first")
+        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/{instance_id}/first")
     if len(result.data) > 0:
         return result.data[0]
     return None
@@ -26,5 +26,5 @@ def first_commit(instance_id):
 def clone(from_instance_id, to_instance_id):
     """ clone process memory with instance id """
     result = HttpClient.post(
-        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/memory/clone/{from_instance_id}/{to_instance_id}")
+        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/clone/{from_instance_id}/{to_instance_id}")
     return not result.has_error

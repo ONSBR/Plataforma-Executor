422c612f45346fa17f4b29c839dac0619792463d
Pedro Farias <pedro.barbosarf@gmail.com>
2020-06-06 17:38:20 -0300

    first_commit get process_memory head

--- sdk/process_memory.py
+++ sdk/process_memory.py
@@ -18,7 +18,7 @@ def create_memory(process_instance_id, event):
 def first_commit(instance_id):
     """ return first commit from instance id  """
     result = HttpClient.get(
-        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/{instance_id}/first")
+        f"{settings.PROCESS_MEMORY_URL}:{settings.PROCESS_MEMORY_PORT}/{instance_id}/head")
     if len(result.data) > 0:
         return result.data[0]
     return None
